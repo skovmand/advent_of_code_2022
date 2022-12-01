1df361b485a5880f25ee2c816dfa7374d540f289
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2022-12-01 16:59:14 +0100

    Init

---
+++ src/stdin.rs
@@ -0,0 +1,28 @@
+use std::{
+    error::Error,
+    fmt::Display,
+    io::{stdin, BufReader, Read},
+};
+
+#[derive(Debug)]
+pub struct StdinReadError;
+
+impl Error for StdinReadError {}
+
+impl Display for StdinReadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Failed to read from stdin.")
+    }
+}
+
+pub fn read_from_stdin() -> String {
+    let stdin = stdin();
+    let mut reader = BufReader::new(stdin.lock());
+    let mut buffer: Vec<u8> = Vec::new();
+
+    reader
+        .read_to_end(&mut buffer)
+        .expect("Failed to read from stdin.");
+
+    String::from(std::str::from_utf8(&buffer).expect("Failed to convert stdin to string."))
+}
