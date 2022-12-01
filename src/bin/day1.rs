1127d30a46552baa13d21864af44019891e07fbb
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2022-12-01 21:28:19 +0100

    Refactor

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,8 +2,9 @@ use std::num::ParseIntError;
 
 use twentytwo::{solution::print_solution, stdin::read_from_stdin};
 
-type ElfWithCalories = Vec<u64>;
-type ElvesWithCalories = Vec<ElfWithCalories>;
+type CalorieCount = u64;
+type ElfWithInventory = Vec<CalorieCount>;
+type ElvesWithCalorieSums = Vec<CalorieCount>;
 
 fn main() {
     let input = read_from_stdin();
@@ -25,10 +26,7 @@ fn main() {
 
 /// D1P1
 fn elf_with_most_calories(input: &str) -> u64 {
-    let elves_with_calories: ElvesWithCalories = parse_input(input).expect("Failed to parse input");
-    let elves_with_calorie_sums: Vec<u64> = elves_with_calorie_sums(elves_with_calories);
-
-    elves_with_calorie_sums
+    parse_input(input)
         .into_iter()
         .max()
         .expect("Could not find max")
@@ -36,29 +34,25 @@ fn elf_with_most_calories(input: &str) -> u64 {
 
 /// D1P2
 fn calorie_sum_of_3_elves_with_most_cals(input: &str) -> u64 {
-    let elves_with_calories: ElvesWithCalories = parse_input(input).expect("Failed to parse input");
-    let mut elves_with_calorie_sums: Vec<u64> = elves_with_calorie_sums(elves_with_calories);
+    let mut elves_with_calorie_sums: Vec<u64> = parse_input(input);
     elves_with_calorie_sums.sort();
     elves_with_calorie_sums.reverse();
 
     elves_with_calorie_sums.iter().take(3).sum()
 }
 
-fn elves_with_calorie_sums(elves: ElvesWithCalories) -> Vec<u64> {
-    elves.iter().map(|elf| elf.iter().sum::<u64>()).collect()
-}
-
-fn parse_input(input: &str) -> Result<ElvesWithCalories, ParseIntError> {
-    let trimmed_input = input.trim();
-
-    trimmed_input
+fn parse_input(input: &str) -> ElvesWithCalorieSums {
+    input
+        .trim()
         .split("\n\n")
         .map(|elf_string| elf_string.split('\n').collect())
         .map(parse_elf_calories)
-        .collect()
+        .map(|elf_with_cals| elf_with_cals.map(|v| v.iter().sum::<u64>()))
+        .collect::<Result<ElvesWithCalorieSums, ParseIntError>>()
+        .expect("Failed to parse input")
 }
 
-fn parse_elf_calories(input: Vec<&str>) -> Result<ElfWithCalories, ParseIntError> {
+fn parse_elf_calories(input: Vec<&str>) -> Result<ElfWithInventory, ParseIntError> {
     input
         .iter()
         .map(|cal| cal.parse::<u64>())
