1a09af7e6b80b3e8086d20e13c0b2eff98ea74fe
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2022-12-03 14:36:13 +0100

    D3P2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,3 +1,5 @@
+use itertools::{Chunk, Itertools};
+use std::vec::IntoIter;
 use twentytwo::{print_solution, read_from_stdin};
 
 fn main() {
@@ -8,6 +10,13 @@ fn main() {
         1,
         "Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?",
         format!("{}", priority_sum_of_supplies(&input)),
+    );
+
+    print_solution(
+        3,
+        2,
+        "Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?",
+        format!("{}", priority_sum_of_badges(&input)),
     );
 }
 
@@ -22,14 +31,6 @@ fn priority_sum_of_supplies(input: &str) -> u64 {
         .sum()
 }
 
-fn parse_rucksacks(input: &str) -> Vec<&[u8]> {
-    input.lines().map(parse_rucksack).collect()
-}
-
-fn parse_rucksack(input: &str) -> &[u8] {
-    input.as_bytes()
-}
-
 fn split_rucksack(rucksack: &[u8]) -> (&[u8], &[u8]) {
     rucksack.split_at(rucksack.len() / 2)
 }
@@ -41,7 +42,37 @@ fn element_in_both_compartments((compartment_a, compartment_b): (&[u8], &[u8])) 
         .expect("Common element")
 }
 
+// D3P2
+fn priority_sum_of_badges(input: &str) -> u64 {
+    parse_rucksacks(input)
+        .into_iter()
+        .chunks(3)
+        .into_iter()
+        .map(elf_badge_element)
+        .map(to_priority_score)
+        .map(u64::from)
+        .sum()
+}
+
+fn elf_badge_element(rucksack_chunk: Chunk<IntoIter<&[u8]>>) -> u8 {
+    match rucksack_chunk.collect::<Vec<&[u8]>>().as_slice() {
+        [a, b, c] => *a
+            .iter()
+            .filter(|elem| b.contains(elem))
+            .find(|elem| c.contains(elem))
+            .expect("Common badge"),
+        _ => panic!("Unexpected rucksack count"),
+    }
+}
+
 // Common
+fn parse_rucksacks(input: &str) -> Vec<&[u8]> {
+    input.lines().map(parse_rucksack).collect()
+}
+
+fn parse_rucksack(input: &str) -> &[u8] {
+    input.as_bytes()
+}
 
 fn to_priority_score(element: u8) -> u8 {
     if (97..=122).contains(&element) {
@@ -76,5 +107,15 @@ CrZsJsPPZsGzwwsLwLmpwMDw"#;
     #[test]
     fn solves_p1() {
         assert_eq!(priority_sum_of_supplies(PUZZLE_INPUT), 8088);
+    }
+
+    #[test]
+    fn solves_p2_example() {
+        assert_eq!(priority_sum_of_badges(EXAMPLE), 70);
+    }
+
+    #[test]
+    fn solves_p2() {
+        assert_eq!(priority_sum_of_badges(PUZZLE_INPUT), 2522);
     }
 }
